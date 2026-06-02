f63830b43b0329c0225c0fb93f98737f940a7a0c
Emily Asadoorian <emilyroseasadoorian@gmail.com>
2026-06-01 23:25:50 -0400

    Add search/scroll functions

--- scripts/index.js
+++ scripts/index.js
@@ -5,3 +5,31 @@ $(window).on("load resize", function() {
         $("#searchBar").outerWidth(Math.min($("#navbar").innerWidth() - ($("#navbar a.me-1").outerWidth(true) * 4.5), ($(window).innerWidth() / 4.5)), true);
     }
 });
+
+$("#searchBar").keypress(function(e) { if (e.which == 13) { searchBlurbs(); } });
+$("#searchIcon").click(function() { searchBlurbs(); } );
+
+function searchBlurbs() {
+    var searchText = new RegExp($("#searchBar").val(), 'gim');
+    $(".blurb").each(function () {
+        var currBlurb = $(this); var matched = false;
+        $(this).find(".poemDetails").each(function () {
+            if (searchText.test($(this).html())) {
+                matched = true; return true;
+            }
+        });
+        if (matched) {
+            currBlurb.removeClass("d-none");
+        } else {
+            currBlurb.addClass("d-none");
+        }
+    });
+}
+
+
+$("#scrollToTop").click(function() {
+    $("html").animate({ scrollTop: 0 }, 600);
+});
+$("#scrollToBottom").click(function() {
+    $("html").animate({ scrollTop: document.body.scrollHeight }, 600);
+});
